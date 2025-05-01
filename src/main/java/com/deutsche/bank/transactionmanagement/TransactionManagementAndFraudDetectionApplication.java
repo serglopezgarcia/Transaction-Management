675c9e4613f1585bc5195490b320e7ca162f078a
Y320528 <sergionicolas.lopezgarcia@nttdata.com>
2025-05-01 22:03:39 +0200

    Initial commit. Project setup

---
+++ src/main/java/com/deutsche/bank/transactionmanagement/TransactionManagementAndFraudDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.deutsche.bank.transactionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionManagementAndFraudDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionManagementAndFraudDetectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deutsche/bank/transactionmanagement/TransactionManagementAndFraudDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deutsche.bank.transactionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionManagementAndFraudDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
