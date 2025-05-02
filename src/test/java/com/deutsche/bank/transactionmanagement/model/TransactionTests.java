66c31b0a4c2a25ae339a3cd4d83e97e416fe1601
N890301 <sergionicolas.lopezgarcia@nttdata.com>
2025-05-02 13:42:53 +0200

    Transaction Model Test Class

---
+++ src/test/java/com/deutsche/bank/transactionmanagement/model/TransactionTests.java
@@ -0,0 +1,38 @@
+package com.deutsche.bank.transactionmanagement.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TransactionTests {
+
+    @Test
+    public void testTransactionCreationObject() {
+        Transaction transaction = new Transaction();
+        transaction.setId(1L);
+        transaction.setAccountNumber("05052025");
+        transaction.setTransactionType("Debit");
+        transaction.setAmount(new BigDecimal("50"));
+        transaction.setTransactionTimestamp(LocalDateTime.now());
+
+        assertEquals(1L, transaction.getId());
+        assertEquals("05052025", transaction.getAccountNumber());
+        assertEquals("Debit", transaction.getTransactionType());
+        assertEquals(new BigDecimal("50"), transaction.getAmount());
+        assertNotNull(transaction.getTransactionTimestamp());
+    }
+
+    @Test
+    public void testTransactionTimestamp() {
+        LocalDateTime timestamp = LocalDateTime.of(2025, 05, 2, 13, 0);
+        Transaction transaction = new Transaction();
+        transaction.setTransactionTimestamp(timestamp);
+
+        assertEquals(timestamp, transaction.getTransactionTimestamp());
+    }
+}
+
